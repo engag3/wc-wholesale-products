b92ca96affccac0c7f131b523e73febfac3fd581
ENGAG3 Media <media@engag3.us>
2016-06-04 08:52:35 -0700

    added github headers. plus gpl2

--- woocommerce-wholesale-products.php
+++ woocommerce-wholesale-products.php
@@ -1,14 +1,31 @@
 <?php
-/**
- * Plugin Name: E3 WooCommerce Wholesale Category
- * Plugin URI: https://www.engag3.media
- * Description: Products in the "Wholesale" category will only be visible to users with the "Wholesale" user role.
- * Version: 1.0.1
- * Author: ENGAG3 Media
- * Author URI: https://www.engag3.media
- * GitHub Plugin URI: engag3/wc-wholesale-products
- * License: GPL2
- */
+/*!
+  Plugin Name: E3 WooCommerce Wholesale Category
+  Plugin URI: https://github.com/engag3/wc-wholesale-products
+  Description: Products in the "Wholesale" category will only be visible to users with the "Wholesale" user role.
+  Version: 1.0.1
+  Author: ENGAG3 Media
+  Author URI: https://www.engag3.media
+  GitHub Plugin URI: https://github.com/engag3/wc-wholesale-products
+  GitHub Branch: master
+
+
+  Copyright 2016  ENGAG3 (email : media@engag3.us)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 
 // Woocommerce - Redirect unauthorised users from accessing a specified product category when clicked or visited via direct url
 
